c43d5e3fd5537ea15d06769136fc1de34ea77206
Arkungking <aryagiffari0@gmail.com>
2025-11-24 15:10:18 +0700

    user module & schema changing

--- src/modules/users/dto/user-update.dto.ts
+++ src/modules/users/dto/user-update.dto.ts
@@ -1,11 +1,6 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsString } from "class-validator";
 
 export class UserUpdateDTO {
-  @IsOptional()
   @IsString()
-  name?: string;
-
-  @IsOptional()
-  @IsString()
-  password?: string;
+  name!: string;
 }
