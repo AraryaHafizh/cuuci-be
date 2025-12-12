fbc61b9b81ca0224895b56eaab9f9943872dd4bf
AraryaHafizh <araryahafizh@gmail.com>
2025-12-12 21:44:19 +0700

    feat: driver module done

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,11 @@ import { AuthRouter } from "./modules/auth/auth.router";
 import { UserUpdateRouter } from "./modules/users/user.router";
 import { AttendanceRouter } from "./modules/attendances/attendance.router";
 import { OutletRouter } from "./modules/outlets/outlet.router";
+import { DriverRouter } from "./modules/drivers/driver.router";
 
 export class App {
   app: Express;
-  
+
   constructor() {
     this.app = express();
     this.configure();
@@ -27,11 +28,13 @@ export class App {
     const userUpdaterouter = new UserUpdateRouter();
     const attendanceRouter = new AttendanceRouter();
     const outletRouter = new OutletRouter();
+    const driverRouter = new DriverRouter();
 
     this.app.use("/auth", authRouter.getRouter());
     this.app.use("/users", userUpdaterouter.getRouter());
     this.app.use("/attendances", attendanceRouter.getRouter());
     this.app.use("/outlets", outletRouter.getRouter());
+    this.app.use("/drivers", driverRouter.getRouter());
   }
 
   private handleError() {
