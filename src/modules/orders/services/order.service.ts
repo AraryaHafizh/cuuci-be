2c3768604ab4af9fb1b1f1aad61983b32ea744f2
Arkungking <aryagiffari0@gmail.com>
2025-12-18 12:55:10 +0700

    pull 18 dec

--- src/modules/orders/services/order.service.ts
+++ src/modules/orders/services/order.service.ts
@@ -1,6 +1,8 @@
+import { OrderStatus, Prisma } from "../../../generated/prisma/client";
+import { AuthUserData } from "../../../types/auth-user-data";
 import { ApiError } from "../../../utils/api-error";
 import { PrismaService } from "../../prisma/prisma.service";
-import { GetOrderDTO } from "../dto/get-order.dto";
+import { GetOrdersDTO } from "../dto/get-order.dto";
 
 export class OrderService {
   private prisma: PrismaService;
@@ -10,27 +12,142 @@ export class OrderService {
   }
 
   getOrders = async (
-    authUserId: string,
-    // orderId?
-    // status?
-    // outletId?
-    // decode token kemudian di cek id nya sama apa engga == res.locals.users
-    // bisa juga sekalian memfilter order apa yang bisa diakses berdasarkan payload token
-    page = 1,
-    limit = 10,
-    search?: string
+    userData: AuthUserData,
+    status: OrderStatus,
+    query: GetOrdersDTO,
+    body: any
   ) => {
-    const user = await this.prisma.user.findFirst({
-      where: { id: authUserId },
-      include: { outletAdmin: true },
+    const { page, search, limit } = query;
+    const { authUserId, role } = userData;
+    const { isHistory, outletId } = body;
+    const whereClause: Prisma.OrderWhereInput = {};
+    const includeClause: Prisma.OrderInclude = {};
+
+    switch (role) {
+      case "CUSTOMER":
+        whereClause.customerId = authUserId;
+        if (isHistory) {
+          whereClause.status = "COMPLETED";
+        }
+        break;
+
+      case "DRIVER":
+        whereClause.driverId = authUserId;
+        if (isHistory) {
+          whereClause.status = "COMPLETED";
+        }
+        break;
+
+      case "WORKER":
+        includeClause.orderWorkProcesses = true;
+        whereClause.outletId = outletId;
+        if (isHistory) {
+          whereClause.status = "COMPLETED";
+        }
+        break;
+
+      case "OUTLET_ADMIN":
+        whereClause.outletId = outletId;
+        break;
+
+      case "SUPER_ADMIN":
+        includeClause.customer = true;
+        includeClause.address = true;
+        includeClause.outlet = true;
+        includeClause.payment = true;
+        break;
+    }
+
+    if (search) {
+      whereClause.outlet = { name: { contains: search, mode: "insensitive" } };
+    }
+    if (status) {
+      whereClause.status = status;
+    }
+
+    const skip = (page - 1) * limit;
+    const orders = await this.prisma.order.findMany({
+      skip,
+      take: limit,
+      where: whereClause,
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: includeClause,
+    });
+    const total = await this.prisma.order.count({
+      where: whereClause,
     });
 
-    if (!user) {
-      throw new ApiError("User not found", 404)
-    }
+    return {
+      message: "Orders fetched successfully",
+      data: orders,
+      meta: {
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   };
 
-  getOrderDetails = async () => {};
+  getOrderDetail = async (userData: AuthUserData, orderId: string) => {
+    const { authUserId, outletId, role } = userData;
+    const whereClause: Prisma.OrderWhereInput = {};
+    const includeClause: Prisma.OrderInclude = {};
+
+    whereClause.id = orderId;
+    switch (role) {
+      case "CUSTOMER":
+        whereClause.customerId = authUserId;
+        includeClause.orderWorkProcesses = true;
+        includeClause.payment = true;
+        break;
+
+      case "DRIVER":
+        whereClause.driverId = authUserId;
+        includeClause.address = true;
+        includeClause.pickupOrders = true;
+        includeClause.deliveryOrders = true;
+        break;
+
+      case "OUTLET_ADMIN":
+        whereClause.outletId = outletId;
+        includeClause.address = true;
+        includeClause.orderItems = true;
+        includeClause.payment = true;
+        break;
+
+      case "WORKER":
+        whereClause.outletId = outletId;
+        includeClause.orderItems = true;
+        includeClause.orderWorkProcesses = true;
+        break;
+
+      case "SUPER_ADMIN":
+        includeClause.outlet = true;
+        includeClause.customer = true;
+        includeClause.orderItems = true;
+        includeClause.payment = true;
+        break;
+
+      default:
+        throw new ApiError("Unauthorized", 401);
+    }
+
+    const order = await this.prisma.order.findFirst({
+      where: whereClause,
+      include: includeClause,
+    });
+    if (!order) {
+      throw new ApiError("Order not found", 404);
+    }
+
+    return {
+      message: "Order fetched successfully",
+      data: order,
+    };
+  };
 
   updateOrderStatus = async () => {};
 
@@ -43,5 +160,4 @@ export class OrderService {
   autoDeleteUnverifiedUser = async () => {};
   autoCancelUnpaidOrders = async () => {};
   autoConfirmDeliveredOrders = async () => {};
-
 }
